1953cf1d61b2a2ed3e3ad23b99bfe8e44789d591
olya-s <osafronyuk85@gmail.com>
2021-07-04 22:57:18 +0300

    add mockup data

---
+++ mockup-data.js
@@ -0,0 +1,59 @@
+export let my_data = [
+  [{
+    id: 0,
+    name: 'Shopping list',
+    created: (new Date(2020, 1, 1)).toDateString(),
+    category: 'Task',
+    content: 'Tomatoes, bread',
+    dates: ''
+  },
+  {
+    id: 1,
+    name: 'Smart thought',
+    created: (new Date(2020, 5, 17)).toDateString(),
+    category: 'Random Thought',
+    content: 'One, looking into a puddle, sees dirt in it, and the other - stars reflected in it',
+    dates: ''
+  },
+  {
+    id: 2,
+    name: 'Thought',
+    created: (new Date(2020, 10, 10)).toDateString(),
+    category: 'Random Thought',
+    content: 'The person who has never been wrong has never tried something new',
+    dates: ''
+  },
+  {
+    id: 3,
+    name: 'New Feature',
+    created: (new Date(2021, 1, 1)).toDateString(),
+    category: 'Idea',
+    content: 'Implement a new feature from 4/07/21 to 5/07/21',
+    dates: '4/07/21, 5/07/21'
+  },
+  {
+    id: 4,
+    name: 'Smart thought',
+    created: (new Date(2021, 1, 2)).toDateString(),
+    category: 'Random Thought',
+    content: 'Life is like riding a bicycle. To keep your balance, you have to move. - Albert Einstein',
+    dates: ''
+  },
+  {
+    id: 5,
+    name: 'Books',
+    created: (new Date(2021, 4, 13)).toDateString(),
+    category: 'Task',
+    content: 'The learn startup',
+    dates: ''
+  },
+  {
+    id: 6,
+    name: 'First Task',
+    created: (new Date(2021, 4, 19)).toDateString(),
+    category: 'Task',
+    content: 'Complete tasks before 7.07.21',
+    dates: '7.07.21'
+  }
+  ]
+];
